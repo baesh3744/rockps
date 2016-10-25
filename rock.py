a8fc5ef9193cc9a79217f4b65eaaa87e20a28c11
baesh3744 <baesh3744@naver.com>
2016-10-25 17:01:37 +0900

    add user name

--- rock.py
+++ rock.py
@@ -35,11 +35,13 @@ Rock-Paper-Scissors
 import random
 
 def rocksp():
+    user_name = input("사용자의 이름을 입력해주세요. ")
     while True:
         hand = ["가위", "바위", "보"]
         com_hand = int(random.randrange(3))
         user_hand = int(input("무엇을 내시겠습니까? (0:가위, 1:바위, 2:보) "))
         print("컴퓨터가 낸 것은 "+ hand[com_hand]+ " 입니다.")
+        print(user_name + "이 낸 것은 " + hand[user_hand] + " 입니다.")
         if (com_hand==user_hand):
             print("Draw!!!")
         elif ((user_hand-com_hand==1) or (user_hand-com_hand==-2)):
